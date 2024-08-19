fc2669465c5556ccc6081468bde94ac4fe6c7d35
minion427 <138232305+minion427@users.noreply.github.com>
2024-08-19 16:48:02 +0900

    Feature/challenge (#8)
    * divide directories
    
    * divide directories
    
    * chore: resolve conflict
    
    * feat: add routing

--- challenges/forms/ChallengeForm.py
+++ challenges/forms/ChallengeForm.py
@@ -6,14 +6,13 @@ class challengeForm(forms.ModelForm):
     class Meta:
         model = Challenges
         fields = ('challenge_title', 'created_username', 'challenge_content', 'board',
-                  'created_date', 'start_date','end_date', 'duration')
+                   'start_date','end_date', 'duration')
 
         labels = {
             'challenge_title': '챌린지 이름',
             'created_username': '작성자명',
             'challenge_content': '챌린지내용',
             'board': '장/단기 선택',
-            'created_date': '챌린지 생성일',
             'start_date': '시작일',
             'end_date': '종료일',
             'duration': '기간'
