59cbf8a6f01a81a85affee4a62ffb846ca4053e8
forlyby <forlyby@naver.com>
2024-08-19 14:08:06 +0900

    feat: implement login feature
    ChallengeUrls.py
    - add login feature

--- challenges/view/UserViews.py
+++ challenges/view/UserViews.py
@@ -1,15 +1,5 @@
-from .models import *
-from accounts.models import *
-import json
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from django.contrib.auth.models import User
-from .forms import challengeForm
-
-from django.core.paginator import Paginator
+#from .models import *
 from django.shortcuts import render
-from django.shortcuts import redirect
-from django.views import View
 from django.views.decorators.csrf import csrf_protect
 from django.middleware.csrf import get_token
 from django.http import JsonResponse, HttpRequest, HttpResponse
@@ -21,12 +11,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
-
-
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
-from django.utils import timezone
-from challenges.models import Challenges, Users
 import json
 # 0.Test code-----------------------------------------------------------------------------------------------------
 def test(request: HttpRequest) -> HttpResponse:
@@ -155,34 +139,3 @@ def delete_account_view(request, username):
         return JsonResponse({'message': '회원 탈퇴 성공'}, status=200)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
-
-
-def challenge_list(request):
-    challengelist = Challenges.objects.all()
-    return render(request, 'challenges/challengeList.html', {'challengelist': challengelist})
-
-
-def challenge_detail(request, challenge_id):
-    challenge = get_object_or_404(Post, pk=challenge_id)
-    return render(request, 'challenges/글열람.html', {'challenge': challenge})
-
-
-def challenge_create(request):
-    if request.method == "POST":
-        if form.is_valid():
-            challenge = form.save(commit=False)
-            challenge.author = request.user
-            challenge.created_date = timezone.now()
-
-            challenge.save()  #DB에 적용
-
-            # 방금 생성한 PK와 일치하는 챌린지의 상세페이지로 이동
-            return redirect('challenge_detail', pk=challenge.pk)
-
-    # POST 요청이 없는 경우
-    else:
-        # 빈 폼 전달
-        form = challengeForm()
-
-    # 챌린지 생성 페이지
-    return render(request, 'challenges/챌린지 생성 상세.html', {'form': form})
